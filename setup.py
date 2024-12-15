e17bdb012e6b052884a89991fb4d89671173a54f
Matthew Harrison <matt@harrison.consulting>
2024-12-15 10:25:17 -0500

    Add GitHub Actions workflow for publishing to PyPI and update setup.py for dynamic versioning

--- setup.py
+++ setup.py
@@ -1,30 +1,39 @@
-from setuptools import setup, find_packages
+import setuptools
+from setuptools import setup
+import os
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+# Read the contents of your README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# Get version from git tag
+version = os.environ.get('GITHUB_REF_NAME', '0.0.0').lstrip('v')
 
 setup(
-    name='SummarizeGPT',
-    version='1.2',
-    packages=find_packages(),
-    description='Tool to summarize directories of code for prompting with LLMs',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Matt Harrison',
-    author_email='matt@harrison.consulting',
-    license='GPLv3',
-    url='https://github.com/Maralai/SummarizeGPT',
+    name="SummarizeGPT",
+    version=version,
+    author="Matt Harrison",
+    author_email="matt@harrison.consulting",
+    description="Tool to summarize directories of code for prompting with LLMs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Maralai/SummarizeGPT",
+    packages=setuptools.find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
-    install_requires=required,
+    install_requires=[
+        'gitignore_parser',
+        'tiktoken'
+    ],
     entry_points={
         'console_scripts': [
             'SummarizeGPT=summarizeGPT.summarizeGPT:main',
         ],
     },
+    license="GPLv3",
 )
